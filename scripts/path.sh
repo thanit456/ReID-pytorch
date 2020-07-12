945c9a94eff7c2d426b04501157f5ff891c42a54
thanit456 <too_w_o@hotmail.com>
2020-07-12 14:32:02 +0700

    can run baseline on cpu but still not cleaned all

---
+++ scripts/path.sh
@@ -0,0 +1,12 @@
+export PATH="${PWD}/configs:$PATH"
+export PATH="${PWD}/config:$PATH"
+export PATH="${PWD}/data:$PATH"
+export PATH="${PWD}/engine:$PATH"
+export PATH="${PWD}/layers:$PATH"
+export PATH="${PWD}/modeling:$PATH"
+export PATH="${PWD}/model:$PATH"
+export PATH="${PWD}/solver:$PATH"
+export PATH="${PWD}/tests:$PATH"
+export PATH="${PWD}/tools:$PATH"
+export PATH="${PWD}/utils:$PATH"
+
